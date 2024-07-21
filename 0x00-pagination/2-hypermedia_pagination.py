fb61cf85f973cdb86f97dc3f29848685e0f43831
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-07-21 17:50:15 +0200

    21-7-2024

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""2"""
+"""Pagination2.
+"""
 
 import csv
 import math
@@ -7,20 +8,23 @@ from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """index_range"""
+    """index_range.
+    """
 
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
-    """Server"""
+    """Server.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """dataset"""
+        """dataset.
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,7 +34,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """get_page"""
+        """get_page.
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
@@ -40,7 +45,8 @@ class Server:
         return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """get_hyper"""
+        """get_hyper.
+        """
         data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
