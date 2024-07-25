8945ab2d5c0f21ddb83f2d8355e155036b5f86fe
Salwa Mahmoud <108579776+SalwaMahmoud9@users.noreply.github.com>
2024-07-25 20:06:52 +0300

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""4.
+"""4
 """
 from collections import OrderedDict
 
@@ -7,16 +7,17 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """MRUCache.
+    """MRUCache
     """
+
     def __init__(self):
-        """Initialize.
+        """init
         """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """put.
+        """put
         """
         if key is None or item is None:
             return
@@ -30,7 +31,7 @@ class MRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """get.
+        """get
         """
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
