d41f14d268409be2305fb9cd049c3e5d9c828684
Salwa Mahmoud <108579776+SalwaMahmoud9@users.noreply.github.com>
2024-07-25 20:00:53 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python3
-
-'''1.
-'''
-
-
+"""1
+"""
 from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''FIFOCache.
-    '''
+    """FIFOCache
+    """
 
     def __init__(self):
+        """init
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        '''put.
-        '''
-
+        """put
+        """
         if key is None or item is None:
             return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
         self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        '''get.
-        '''
+        """get
+        """
         return self.cache_data.get(key, None)
